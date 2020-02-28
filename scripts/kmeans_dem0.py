31307084e44bd985289ece1834109c31273dbca9
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2020-02-28 17:10:53 -0500

    development

--- scripts/kmeans_dem0.py
+++ scripts/kmeans_dem0.py
@@ -10,28 +10,30 @@ d3=np.random.normal(7,.5,(8,3))
 
 d = np.vstack((d1,d2,d3))
 
-centroids=2
+centroids=3
 
 c=np.random.normal(np.mean(d),np.std(d),(centroids,d.shape[1]))
 
 
-def mydist(d,c):
-    distarray=[]
-    for i in range(c.shape[0]):
-        distarray.append(np.sum((d-c[i])**2,axis=1)**.5)
+def kmeans(dat,centroids,max_iter):
     
-    distarray=np.array(distarray)
-    return distarray
+    d = dat    
+    c=np.random.normal(np.mean(d),np.std(d),(centroids,d.shape[1]))
     
-for j in range(16):
-    dists = mydist(d,c).T
-    clusts=np.argmin(dists,axis=1)
-    for i in range(centroids):
-        c[i]=np.mean(d[clusts==i],axis=0)
-    
-    print(clusts)
-    print(c)
-    
-    
-    
-    
+    def mydist(d,c):
+        distarray=[]
+        for i in range(c.shape[0]):
+            distarray.append(np.sum((d-c[i])**2,axis=1)**.5)
+        
+        distarray=np.array(distarray)
+        return distarray   
+        
+    for j in range(16):
+        dists = mydist(d,c).T
+        clusts=np.argmin(dists,axis=1)
+        for i in range(centroids):
+            c[i]=np.mean(d[clusts==i],axis=0)
+        
+    return clusts
+        
+kmeans(d,3,16)
