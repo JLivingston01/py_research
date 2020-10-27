07378d9dbb0d0cfdad092f975e6719c616473f9d
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2020-10-26 21:48:48 -0400

    image analytics tensorflow

--- scripts/heart_modeling_reweighted_knn_parallel.py
+++ scripts/heart_modeling_reweighted_knn_parallel.py
@@ -1,42 +1,81 @@
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Load Data, Fill NA with column means and Standardize
+dat = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data",header=None,sep="\t")[0]
+
+X=pd.DataFrame(np.array(list(dat.str.split())))
+
+for i in X.columns.values:
+    X[i]=X[i].replace("?",np.nan).astype(float)
+    X[i].fillna(np.nanmean(X[i]),inplace=True)
+    
+Y=np.array(X[0])
+X=X[[i for i in X.columns.values if i>0]]
 
 
-X = np.array([[1,2,3],
-              [3,2,1],
-              [2,1,3],
-              [1,3,2]])
+X=(X-np.mean(X))/np.std(X)
+X['int']=1
+X=np.array(X)
 
-
-B = [2,3,-3]
-
-Y = X@B
-
-
+#Initialize 600 chains of 30,000 iterations to evaluate 8 model coefficients, with regressions scored by the R-squared.
 draws = 600
-w0=np.random.normal(0,1,(3,draws))
+w0=np.random.normal(0,.01,(X.shape[1],draws))
 
-score0=1-np.sum((Y.reshape(4,1)-X@w0)**2,axis=0)/sum((Y-np.mean(Y))**2)
+score0=1-np.sum((Y.reshape(len(Y),1)-X@w0)**2,axis=0)/sum((Y-np.mean(Y))**2)
 
-delta=np.zeros((3,draws))
+delta=np.zeros((X.shape[1],draws))
 stepsize=.0001
 
 updates = 0
-while updates < 10000:
+while updates < 30000:
     w1=w0+np.random.normal(delta,stepsize)
-    
-    score1=1-np.sum((Y.reshape(4,1)-X@w1)**2,axis=0)/sum((Y-np.mean(Y))**2)
-    
+    score1=1-np.sum((Y.reshape(len(Y),1)-X@w1)**2,axis=0)/sum((Y-np.mean(Y))**2)
     
     delta = np.where(score1>score0,w1-w0,delta)
     w0=np.where(score1>score0,w1,w0)
     print(sum(np.where(score1>score0,1,0)))
     score0=score1
-    #score0=np.where(score1>score0,score1,score0)
     updates+=1
 
 
-np.mean(w0,axis=1)
+coef_est=np.round(np.mean(w0,axis=1),2)
+print(coef_est)
+
+
+
+coef_actual=np.round(np.linalg.inv(X.T@X)@(X.T@Y),2)
+print(coef_actual)
+
+
+def dist_plot(i, fontsize=12):
+    plt.hist(w0[i],bins=30,label='est. distribution')
+    plt.bar(coef_actual[i],100,color='.1',width=1,alpha=.5,label='true coef')
+    plt.ylim((0,60))
+    plt.legend()
+    plt.title("feat_"+str(i)+"; Mean: "+str(coef_est[i])+", Exact: "+str(coef_actual[i]))
+    
+    
+fig = plt.figure(figsize=(10,15))
+ax1 = fig.add_subplot(4,2,1)
+ax1 = dist_plot(0, fontsize=12)
+ax2 = fig.add_subplot(4,2,2)
+ax2 = dist_plot(1, fontsize=12)
+ax3 = fig.add_subplot(4,2,3)
+ax3 = dist_plot(2, fontsize=12)
+ax4 = fig.add_subplot(4,2,4)
+ax4 = dist_plot(3, fontsize=12)
+ax5 = fig.add_subplot(4,2,5)
+ax5 = dist_plot(4, fontsize=12)
+ax6 = fig.add_subplot(4,2,6)
+ax6 = dist_plot(5, fontsize=12)
+ax7 = fig.add_subplot(4,2,7)
+ax7 = dist_plot(6, fontsize=12)
+ax8 = fig.add_subplot(4,2,8)
+ax8 = dist_plot(7, fontsize=12)
+plt.show()
 
 
 #KNN
